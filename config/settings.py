460e93ba959ebf388b00d336f99ebce285b1cf76
CodeByDouglas <douglas.ofc.2004@gmail.com>
2026-01-27 11:34:53 -0300

    feat(cache): configura redis para django e atualiza dependencias

--- config/settings.py
+++ config/settings.py
@@ -88,6 +88,18 @@ DATABASES = {
     }
 }
 
+# Cache
+# https://docs.djangoproject.com/en/6.0/topics/cache/
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.getenv("DJANGO_REDIS_URL", "redis://localhost:6379/1"),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
